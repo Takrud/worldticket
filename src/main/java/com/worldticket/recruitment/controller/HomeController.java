b41166d5c4b0c31ab6f11d5472880ddd4cea748f
Siripha_ta <siripha.ta@tnis.com>
2017-07-23 14:09:45 +0700

    Initial commit

---
+++ src/main/java/com/worldticket/recruitment/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.worldticket.recruitment.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String home(Model model) {
+        model.addAttribute("name", "Foo Bar");
+        return "home";
+    }
+
+
+}
